399f60e793e708cea253093e98871113907ed3b6
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-23 18:29:40 +0500

    file storage in public folder connected database

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-/* configurations */ 
+/* CONFIGURATIONS */
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename); // this configuration is used when we are using es6 instead of commonjs
 dotenv.config();
@@ -18,9 +19,29 @@ const app = express();
 app.use(express.json());
 
 app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"})); // for using different third party cockies or resources
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // for using different third party cockies or resources
 app.use(morgan("common")) // logger middleware
-app.use(bodyParser.json({limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors()) // invokes the cross origin resource sharing policy
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+const port = process.env.PORT || 5001
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    app.listen(port, () => {
+        console.log(`Server is listening to port ${port}`);
+    })
+}).catch((err) => console.log(`${err} did not connect`))
