1978430f7b6ba428db3cbfca3cf54fa94dc9dda8
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-11-14 10:37:04 +0500

    updated metadata

--- server/index.js
+++ server/index.js
@@ -26,6 +26,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // for using different third party cockies or resources
+app.use(morgan("common")) // logger middleware
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors()) // invokes the cross origin resource sharing policy
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 // Custom CORS Headers
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -40,14 +47,6 @@ app.use((req, res, next) => {
     }
     next();
 });
-
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // for using different third party cockies or resources
-app.use(morgan("common")) // logger middleware
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors()) // invokes the cross origin resource sharing policy
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
