08501900a428aa43eee97ca33b6a603fa789f022
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-11-14 12:53:38 +0500

    unique key to friends list

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -38,9 +38,9 @@ const FriendListWidget = ({ userId }) => {
         Friend List
       </Typography>
       <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends?.map((friend) => (
+        {friends?.map((friend, index) => (
           <Friend
-            key={friend._id}
+            key={friend._id || index}
             friendId={friend._id}
             name={`${friend.firstName} ${friend.lastName}`}
             subtitle={friend.occupation}
